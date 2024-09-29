30e2340949bc730e1f70abca0d7b772ed2c88c96
Latiful Rahman <latif.fiverr@gmail.com>
2024-09-29 16:13:06 +0000

    add process and fetch data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+
+// get data by location
+const fetchData = async (location) => {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=EXMJNVS7XJK4WNG6ATHXBUBG2&contentType=json`)
+        const data = await response.json();
+        return data;
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
+
+// extract the data that needed
+const processData = async (location) => {
+    const data = await fetchData(location);
+    const { datetime, humidity, conditions, temp, windspeed, feelslike } = data.currentConditions;
+    return { datetime, humidity, conditions, temp, windspeed, feelslike }
+}
+
