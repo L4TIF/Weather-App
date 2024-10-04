45cd442f55a81e171234f22b25686b554b64ff6e
Latiful Rahman <latif.fiverr@gmail.com>
2024-10-04 16:03:30 +0000

    top section done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+
 const form = document.querySelector('form');
 
 // get data by location
@@ -23,8 +24,8 @@ const processData = async (location) => {
     const data = await fetchData(location);
     if (data) {
         const { description, resolvedAddress: address, timezone } = data;
-        const { datetime, humidity, conditions, temp, windspeed, feelslike } = data.currentConditions;
-        return { datetime, humidity, conditions, temp, windspeed, feelslike, description, address, timezone }
+        const { datetime, humidity, conditions, temp, windspeed, feelslike, icon } = data.currentConditions;
+        return { datetime, humidity, conditions, temp, windspeed, feelslike, description, address, timezone, icon }
     }
 }
 
@@ -33,11 +34,16 @@ form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const input = document.querySelector('input').value;
     const result = document.querySelector('.result');
-    result.textContent = 'loading';
+    const location = document.querySelector('.location');
+    const temp = document.querySelector('.temp');
+    const icon = document.querySelector('.icon img');
 
     const dataObj = await processData(input);
     if (dataObj) {
         result.textContent = dataObj.conditions || "no weather data found";
+        location.textContent = dataObj.address;
+        temp.innerHTML = dataObj.temp + '&deg;';
+        icon.src = require(`./assets/${dataObj.icon}.png`)
 
     } else {
         result.textContent = 'invalid city name'
